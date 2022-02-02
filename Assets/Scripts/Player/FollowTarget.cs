0447611a0d06b10ef8016c0e7e0fe0fa0aa1a9ce
M1R4B3L <amirabelmartinez@gmail.com>
2022-02-02 20:25:18 +0100

    Smooth Camera Follow

--- Assets/Scripts/Player/FollowTarget.cs
+++ Assets/Scripts/Player/FollowTarget.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class FollowTarget : MonoBehaviour
 {
     public Transform target;
+    public float smoothSpeed = 10.0f;
     public Vector3 cameraOffset;
+
+    Vector3 currentVel;
 
     void Start()
     {
@@ -13,9 +16,11 @@ public class FollowTarget : MonoBehaviour
     }
 
    
-    void Update()
+    void LateUpdate()
     {
         Vector3 newPos = target.transform.position + cameraOffset;
-        transform.position = newPos;
+        Vector3 smoothPos = Vector3.SmoothDamp(transform.position, newPos, ref currentVel, smoothSpeed * Time.deltaTime);
+        transform.position = smoothPos;
+        transform.LookAt(target);
     }
 }
