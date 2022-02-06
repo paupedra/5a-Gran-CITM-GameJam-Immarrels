81ae943b70177da0cf9666503f4b061a322539e3
Gerard Romeu Vidal <45124641+Gromeu2000@users.noreply.github.com>
2022-02-06 02:32:02 +0100

    im trash

--- Assets/Scripts/Tutorial/Ui_Assistant.cs
+++ Assets/Scripts/Tutorial/Ui_Assistant.cs
@@ -10,11 +10,16 @@ public class Ui_Assistant : MonoBehaviour
 
     private void Awake()
     {
-        messageText = transform.Find("message").Find("WelcomeText").GetComponent<Text>();
+        messageText = transform.Find("WelcomePanel").Find("UI Assistant").Find("message").Find("WelcomeText").GetComponent<Text>();
     }
 
     private void Start()
     {
-        textWriter.AddWriter(messageText, "Welcome to Tamara.\nMove with WASD and recollect resources with SpaceBar.\nOnce you have collected all resources spend them to unlock the next tile.\nSTART YOUR EXPANSION AND ENJOY!", 10f);
+        textWriter.AddWriter(messageText, "Welcome to Tamara.\nMove with WASD and recollect resources with SpaceBar.\nOnce you have collected all resources spend them to unlock the next tile.\nSTART YOUR EXPANSION AND ENJOY!", 0.1f);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
     }
 }
